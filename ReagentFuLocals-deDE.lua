df1e9d6188df5118ce8c2c9aee27a138d46ba41c
John Pasula <ackis@shaw.ca>
2009-02-03 16:24:56 +0000

    Convert remaining localizations over, add the esES and esMX in case anyone translates them, add .docmeta and a doc file for the main page.

--- ReagentFuLocals-deDE.lua
+++ ReagentFuLocals-deDE.lua
@@ -1,7 +1,5 @@
 local L = AceLibrary("AceLocale-2.2"):new("ReagentFu");
 
 L:RegisterTranslations("deDE", function() return
-
 --@localization(locale="deDE", format="lua_table", handle-unlocalized="ignore", escape-non-ascii=false, same-key-is-true=true)@
-
 end)
